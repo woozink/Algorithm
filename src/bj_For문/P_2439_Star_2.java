70978ffbd8ac0fa580fc8470589d5fa87a811c91
woozink <k0109974@naver.com>
2021-11-19 18:52:10 +0900

    11월 19(2)

---
+++ src/bj_For문/P_2439_Star_2.java
@@ -0,0 +1,25 @@
+package bj_For문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_2439_Star_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            }
+
+    }
+}
+
