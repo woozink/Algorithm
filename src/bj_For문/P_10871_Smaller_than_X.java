e3af35e5b5acee9ca14f9992464ed2c251b0391f
woozink <k0109974@naver.com>
2021-11-21 23:59:15 +0900

    11월 21일(2)

--- src/bj_For문/P_10871_Smaller_than_X.java
+++ src/bj_For문/P_10871_Smaller_than_X.java
@@ -8,25 +8,24 @@ import java.util.StringTokenizer;
 public class P_10871_Smaller_than_X {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
 
         StringBuilder sb = new StringBuilder();
 
-        st = new StringTokenizer(br.readLine()," ");
+        st = new StringTokenizer(br.readLine());
 
-        for (int i =0; i <N; i++){
+        for (int i = 0; i < N; i++) {
             int value = Integer.parseInt(st.nextToken());
 
-            if (value < X)
+            if (value < X) {
                 sb.append(value).append(' ');
+            }
         }
         System.out.println(sb);
 
-
     }
+
 }
