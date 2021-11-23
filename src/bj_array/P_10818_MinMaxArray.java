45fcaeb237e9eb1b7bcc897a0688a4a3319b6d8e
woozink <k0109974@naver.com>
2021-11-23 23:48:47 +0900

    11월 23일(2)

---
+++ src/bj_array/P_10818_MinMaxArray.java
@@ -0,0 +1,25 @@
+package bj_array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P_10818_MinMaxArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int index = 0;
+        int [] arr = new int[N];
+        while(st.hasMoreTokens()){
+            arr[index] = Integer.parseInt(st.nextToken());
+            index++;
+        }
+        Arrays.sort(arr);
+        System.out.print(arr[0] + " " + arr[N-1]);
+    }
+}
