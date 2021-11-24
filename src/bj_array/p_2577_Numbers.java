11e5b5ad2cb3b8ae586cefb0e696f09f03b8e868
woozink <k0109974@naver.com>
2021-11-24 22:19:11 +0900

    11월 24일(2)

---
+++ src/bj_array/p_2577_Numbers.java
@@ -0,0 +1,26 @@
+package bj_array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p_2577_Numbers {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[10];
+
+        int val = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+
+        String str = String.valueOf(val);
+
+        for (int i = 0; i < str.length(); i++) {
+            arr[(str.charAt(i) - 48)]++;
+
+        }
+
+        for (int v : arr) {
+            System.out.println(v);
+        }
+    }
+}
