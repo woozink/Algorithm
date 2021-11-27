9f4b120d60222a12345ba221d3008c5acd00f1e4
woozink <k0109974@naver.com>
2021-11-27 19:04:43 +0900

    11월 27일(1)

---
+++ src/bj_array/P_4344_OverAverage.java
@@ -0,0 +1,42 @@
+package bj_array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P_4344_OverAverage {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr;
+
+        int testcase = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i =0; i< testcase; i++){
+            st= new StringTokenizer(br.readLine(), " ");
+
+            int N = Integer.parseInt(st.nextToken());
+            arr = new int[N];
+
+            double sum =0;
+
+            for (int j =0; j<N; j++){
+                int val = Integer.parseInt(st.nextToken());
+                arr[j] = val;
+                sum += val;
+            }
+            double mean = (sum / N);
+            double count =0;
+
+            for (int j =0; j <N; j++){
+                if (arr[j]>mean){
+                    count++;
+                }
+            }
+            System.out.printf("%.3f%%\n",(count/N)*100);
+        }
+
+    }
+}
