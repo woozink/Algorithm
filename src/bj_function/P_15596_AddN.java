f5db657831241c1a62ea97aa80319cb63ee3f084
woozink <k0109974@naver.com>
2021-11-28 14:17:14 +0900

    11월 28일(1)

---
+++ src/bj_function/P_15596_AddN.java
@@ -0,0 +1,21 @@
+package bj_function;
+
+class Test {
+    long sum(int[] a){
+        long sum =0;
+
+        for(int i =0 ; i<a.length; i++){
+            sum += a[i];
+        }
+        return sum;
+    }
+
+}
+
+
+
+public class P_15596_AddN {
+    public static void main(String[] args) {
+
+    }
+}
