127369303b90058824eb8ce75d09ce54688f488e
woozink <k0109974@naver.com>
2021-11-29 15:31:10 +0900

    11월 29일(3)

---
+++ src/bj_Retry_Array_Function/P_2562_Max.java
@@ -0,0 +1,31 @@
+package bj_Retry_Array_Function;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_2562_Max {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = 0;
+        int index = 0;
+        int count = 0;
+
+        for (int value : arr) {
+            count++;
+            if (value > max) {
+                max = value;
+                index = count;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
