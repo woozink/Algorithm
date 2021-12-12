c6492d8bda2679a4f8a634cd885f5c6675b0d238
woozink <k0109974@naver.com>
2021-12-12 18:25:44 +0900

    12월 12일(1)

--- src/bj_character/P_2941_Croatia.java
+++ src/bj_character/P_2941_Croatia.java
@@ -23,9 +23,7 @@ public class P_2941_Croatia {
                     //i+1 까지가 하나의 문자이므로 다음 문자를 건너 뛰기 위해 1증가
                     i++;
                 }
-            }
-
-            else if (ch == 'd' && i < len - 1) {
+            } else if (ch == 'd' && i < len - 1) {
                 if (str.charAt(i + 1) == '-') {
                     i++;
                 } else if (str.charAt(i + 1) == 'z' && i < len - 2) {
@@ -34,15 +32,11 @@ public class P_2941_Croatia {
                         i += 2;
                     }
                 }
-            }
-
-            else if ((ch == 'l' || ch == 'n') && i<len -1){
+            } else if ((ch == 'l' || ch == 'n') && i < len - 1) {
                 if (str.charAt(i + 1) == 'j') {
                     i++;
                 }
-            }
-
-            else if ((ch == 's' || ch == 'z') && i<len -1){
+            } else if ((ch == 's' || ch == 'z') && i < len - 1) {
                 if (str.charAt(i + 1) == '=') {
                     i++;
                 }
@@ -53,6 +47,5 @@ public class P_2941_Croatia {
 
         System.out.println(count);
     }
-
 }
 
