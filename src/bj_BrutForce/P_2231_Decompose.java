efca89f03bb134edbb90907c1aab2991ebdd52ae
woozink <k0109974@naver.com>
2022-01-03 13:56:50 +0900

    1월 3일(1)

---
+++ src/bj_BrutForce/P_2231_Decompose.java
@@ -0,0 +1,33 @@
+package bj_BrutForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class P_2231_Decompose {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int result = 0;
+
+        for(int i = 0; i<N; i++){
+            int number =i;
+            int sum =0;
+
+            while(number != 0){
+                sum += number %10;
+                number /= 10;
+            }
+
+            if(sum + i ==N){
+                result =i;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
