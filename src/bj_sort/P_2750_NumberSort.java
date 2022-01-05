535071e68fd7bcace1cde5853fc82ae45e4e91ed
woozink <k0109974@naver.com>
2022-01-05 14:05:17 +0900

    1월 5일(1)

---
+++ src/bj_sort/P_2750_NumberSort.java
@@ -0,0 +1,33 @@
+package bj_sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_2750_NumberSort {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i =0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i =0; i < N -1; i++){
+            for( int j = i+1; j < N; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+
+        for(int val : arr){
+            System.out.println(val);
+        }
+    }
+}
