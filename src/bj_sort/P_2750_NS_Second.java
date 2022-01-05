cc4c0819c95b8e1b4bb0c4d0732adf532052ef68
woozink <k0109974@naver.com>
2022-01-05 14:15:34 +0900

    1월 5일(2)

---
+++ src/bj_sort/P_2750_NS_Second.java
@@ -0,0 +1,26 @@
+package bj_sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Arrays;
+
+public class P_2750_NS_Second {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i =0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int val : arr){
+            System.out.println(val);
+        }
+    }
+}
