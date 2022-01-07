0b9130684b426dab79f32d142c79dbe11b34d67d
woozink <k0109974@naver.com>
2022-01-07 20:43:27 +0900

    1월 7일(1)

---
+++ src/bj_sort/P_1427_SortInside.java
@@ -0,0 +1,21 @@
+package bj_sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P_1427_SortInside {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] arr = br.readLine().toCharArray();
+
+        Arrays.sort(arr);
+
+        for(int i = arr.length -1; i >= 0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
