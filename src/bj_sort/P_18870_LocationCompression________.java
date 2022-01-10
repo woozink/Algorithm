0d41330d1cfab663e2855d5395a49ecf839588d7
woozink <k0109974@naver.com>
2022-01-10 13:56:54 +0900

    1월 10일(1)

--- src/bj_sort/P_18870_LocationCompression________.java
+++ src/bj_sort/P_18870_LocationCompression________.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class P_18870_LocationCompression {
+public class P_18870_LocationCompression________ {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
