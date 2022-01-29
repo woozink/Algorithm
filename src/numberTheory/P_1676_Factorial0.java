a1095adbfc0ebcff4d8c7597964ad370154326b9
woozink <k0109974@naver.com>
2022-01-29 17:46:20 +0900

    1월 29일(3)

---
+++ src/numberTheory/P_1676_Factorial0.java
@@ -0,0 +1,20 @@
+package numberTheory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_1676_Factorial0 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while(num >= 5){
+            count += num / 5;
+            num/=5;
+        }
+        System.out.println(count);
+    }
+}
