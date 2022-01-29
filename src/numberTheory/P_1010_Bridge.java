a21d6dde2eb1908ec042d475161339d8d649eddb
woozink <k0109974@naver.com>
2022-01-29 17:09:27 +0900

    1월 29일(1)

---
+++ src/numberTheory/P_1010_Bridge.java
@@ -0,0 +1,46 @@
+package numberTheory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P_1010_Bridge {
+    static int[][] dp = new int[30][30];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            //M 개중에서 N개를 뽑는 경우이므로 nCr 에서 각각 배정
+            int N = Integer.parseInt(st.nextToken()); // N =r
+            int M = Integer.parseInt(st.nextToken()); // M = n
+
+            sb.append(combi(M, N)).append('\n');
+
+        }
+        System.out.println(sb);
+    }
+
+    static int combi(int n, int r) {
+
+        //이미 풀린 경우 바로 반환
+        if (dp[n][r] > 0) {
+            return dp[n][r];
+        }
+        // 2번 성질
+        if (n == r || r == 0) {
+            return dp[n][r] = 1;
+        }
+        //1번 성질
+        return dp[n][r] = combi(n - 1,r - 1) + combi(n - 1, r);
+    }
+}
