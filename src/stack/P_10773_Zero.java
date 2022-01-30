466a91fa819f04a2d0df5ad95b5ec2cb7de28483
woozink <k0109974@naver.com>
2022-01-30 18:26:18 +0900

    1월 30(2) using  stack class

---
+++ src/stack/P_10773_Zero.java
@@ -0,0 +1,35 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class P_10773_Zero {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer>stack = new Stack<Integer>();
+
+        int K = Integer.parseInt(br.readLine());
+
+        for(int i =0; i < K; i++){
+            int number = Integer.parseInt(br.readLine());
+
+            if(number == 0){// 0이라면 스택에 저장된 top원소를 지운다.
+                stack.pop();
+            }
+            else{
+                //ex) stack.add(numnber);
+                stack.push(number);
+            }
+        }
+        int sum = 0;
+
+        for(int x : stack) {
+            sum += x;
+        }
+
+        System.out.println(sum);
+    }
+}
