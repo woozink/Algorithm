9bb6d5554bb1967eb1a55a70e61601e9747e858d
woozink <k0109974@naver.com>
2022-02-07 13:06:25 +0900

    2월 7일(1)

---
+++ src/bj_binearysearch/P_2805_TreeCut.java
@@ -0,0 +1,45 @@
+package bj_binearysearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P_2805_TreeCut {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] tree = new int[N];
+
+        int min = 0;
+        int max = 0;
+
+        st = new StringTokenizer(br.readLine()," ");
+        for (int i = 0; i < N; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+            if (max < tree[i]) max = tree[i];
+        }
+
+        while (min < max) {
+            int mid = (min + max) / 2;
+            long sum = 0;
+            for (int treeHeight : tree) {
+                if (treeHeight - min > 0) {
+                    sum += (treeHeight - mid);
+                  }
+                if (sum < M) {
+                    max = mid;
+                } else {
+                    min = mid + 1;
+                }
+            }
+        }
+        System.out.println(min - 1);
+    }
+}
