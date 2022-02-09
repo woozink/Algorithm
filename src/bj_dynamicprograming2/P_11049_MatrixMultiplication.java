2af4ba7f1eeaf835f0b822178edc4e2ef88004ae
woozink <k0109974@naver.com>
2022-02-09 14:38:36 +0900

    2월 9일(2)

---
+++ src/bj_dynamicprograming2/P_11049_MatrixMultiplication.java
@@ -0,0 +1,47 @@
+package bj_dynamicprograming2;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P_11049_MatrixMultiplication {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] mat = new int[n][2];
+        StringTokenizer st;
+
+        for(int i =0; i < n; i++){
+            st= new StringTokenizer(br.readLine()," ");
+            mat[i][0] = Integer.parseInt(st.nextToken());
+            mat[i][1] = Integer.parseInt(st.nextToken());
+        }
+        bw.write(String.valueOf(solution(mat)));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+
+    static int solution(int[][] mat){
+        int size = mat.length;
+        int [][] dp = new int[size][size];
+
+        for(int i =0; i < size -1; i ++){
+            dp[i][i+1] =mat[i][0] * mat[i][1] * mat[i+1][1];
+        }
+
+        for(int gap =2; gap < size; gap++){
+            for(int i = 0; i +gap < size; i++){
+                int j = i +gap;
+                dp[i][j] = Integer.MAX_VALUE;
+
+                for(int k = i; k < j; k++){
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k+1][j] +(mat[i][0] * mat[k][1] *mat[j][1]));
+                }
+            }
+        }
+        return dp[0][size-1];
+    }
+}
