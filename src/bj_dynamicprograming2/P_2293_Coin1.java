099eb1053737a7b37411b73d60fd4ae04881cd24
woozink <k0109974@naver.com>
2022-02-10 16:25:05 +0900

    2월 10일(3)

---
+++ src/bj_dynamicprograming2/P_2293_Coin1.java
@@ -0,0 +1,34 @@
+package bj_dynamicprograming2;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P_2293_Coin1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] coin = new int[n];
+
+        for(int i = 0; i < n; i ++){
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(coin);
+        int[] dp = new int[k+1];
+        dp[0] = 1;
+        for(int i =0; i < n; i ++){
+            for(int j = coin[i]; j <=k; j++){
+                dp[j] += dp[j -coin[i]];
+            }
+        }
+        bw.write(dp[k] + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
